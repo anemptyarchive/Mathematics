2fb06fc4748853df36c9f013b84dd90db8872b8d
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2023-10-29 03:50:01 +0900

    nが0のときに意図しない処理になるのを修正

--- golden_ratio/code/fibonacci_sequence.R
+++ golden_ratio/code/fibonacci_sequence.R
@@ -3,48 +3,13 @@
 
 # 利用パッケージ
 library(tidyverse)
-library(gganimate)
 library(patchwork)
 
-# チェック用
+# パッケージ名の省略用
 library(ggplot2)
 
 
 # フィボナッチ数列の実装 -------------------------------------------------------------
-
-# フィボナッチ数の計算を実装
-fibonacci_val <- function(n) {
-  
-  # フィボナッチ数を取得
-  if(n == 0) {
-    val <- 0
-  } else if(n == 1) {
-    val <- 1
-  } else {
-    # フィボナッチ数を計算
-    val <- Recall(n = n-2) + Recall(n = n-1)
-  }
-  
-  # 値を出力
-  return(val)
-}
-
-# フィボナッチ数の計算を実装
-fibonacci_val <- function(n) {
-  
-  # フィボナッチ数を取得
-  if(n == 0) {
-    val <- 0
-  } else if(n %in% c(1, 2)) {
-    val <- 1
-  } else {
-    # フィボナッチ数を計算
-    val <- Recall(n = n-2) + Recall(n = n-1)
-  }
-  
-  # 値を出力
-  return(val)
-}
 
 # フィボナッチ数の計算を実装
 fibonacci_val <- function(n) {
@@ -58,22 +23,29 @@ fibonacci_val <- function(n) {
   }
 }
 
-
 # フィボナッチ数列の計算を実装
 fibonacci_vec <- function(n) {
   
-  # 受け皿を初期化
-  vec <- rep(0, times = n)
+  # フィボナッチ数を出力
+  if(n == 0) {
+    
+    # 0番目の項のみの場合
+    return(0)
+    
+  } else if(n == 1) {
+    
+    # 1番目までの項の場合
+    return(c(0, 1))
+  }
   
-  # 要素数ごとに処理
-  for(i in 1:n) {
+  # n番目までの項の受け皿を作成
+  vec <- c(0, 1, rep(NA, times = n-1))
+  
+  # 項番号ごとに処理
+  for(i in 2:n) {
     
     # フィボナッチ数を格納
-    if(i == 1) {
-      vec[i+1] <- i
-    } else {
-      vec[i+1] <- sum(vec[c(i-1, i)])
-    }
+    vec[i+1] <- sum(vec[c(i-1, i)])
   }
   
   # 数列を出力
@@ -98,14 +70,13 @@ fib_df <- tibble::tibble(
 
 
 # ラベル用の文字列を作成
-fnc_label <- "list(F[0] == 0, F[1] == 1, F[n] == F[n-2] + F[n-1] ~~ (n >= 2))"
+def_label <- "list(F[0] == 0, F[1] == 1, F[n] == F[n-2] + F[n-1] ~~ (n >= 2))"
 
 # フィボナッチ数を作図
 ggplot() + 
   geom_bar(data = fib_df, 
            mapping = aes(x = n, y = v, fill = factor(n)), 
-           stat = "identity", position = "stack", 
-           show.legend = FALSE) + # 要素数ごとの値
+           stat = "identity", show.legend = FALSE) + # 要素数ごとの値
   geom_hline(data = fib_df, 
              mapping = aes(yintercept = v, color = factor(n)), 
              linewidth = 1, linetype = "dashed", show.legend = FALSE) + # 値ごとの軸目盛線
@@ -115,7 +86,7 @@ ggplot() +
   scale_x_continuous(breaks = 0:n_max, minor_breaks = FALSE) + 
   scale_y_continuous(sec.axis = sec_axis(trans = ~., breaks = fib_vec)) + # 値ラベル
   labs(title = "Fibonacci number", 
-       subtitle = parse(text = fnc_label), 
+       subtitle = parse(text = def_label), 
        x = "n", y = "value")
 
 
@@ -158,7 +129,7 @@ seq_graph <- ggplot() +
   scale_fill_manual(breaks = 0:n_max, values = scales::hue_pal()(n_max+1)) + # (要素数ごとに色付け用の小細工)
   guides(fill = guide_legend(override.aes = list(alpha = 1, linewidth = 0))) + # (全ての要素数を凡例に表示用の小細工)
   labs(title = "Fibonacci sequence", 
-       subtitle = parse(text = fnc_label), 
+       subtitle = parse(text = def_label), 
        color = "n", fill = "n", 
        x = "n", y = "value")
 
@@ -185,42 +156,5 @@ patchwork::wrap_plots(
   seq_graph, stack_graph, 
   nrow = 1, widths = c(n_max+1, 1), guides = "collect"
 )
-
-
-# フィボナッチ数列と黄金比の関係 ------------------------------------------------
-
-# 要素数の最大値を指定
-n_max <- 25
-
-# フィボナッチ数列を作成
-fib_vec <- fibonacci_vec(n = n_max)
-
-# 2項比を格納
-ratio_df <- tibble::tibble(
-  n = 1:n_max, 
-  r = fib_vec[-1] / fib_vec[-(n_max+1)]
-)
-
-# 黄金比を計算
-p <- (1 + sqrt(5)) * 0.5
-
-# ラベル用の文字列を作成
-val_label <- paste0(
-  "list(", 
-  "phi == ", round(p, digits = 5), ", ", 
-  "frac(F[n], F[n-1]) == ", round(fib_vec[n_max+1]/fib_vec[n_max], digits = 5), 
-  ")"
-)
-
-# 2項比の推移を作図
-ggplot() + 
-  geom_hline(mapping = aes(yintercept = p), 
-             color = "red", linetype = "dashed") + # 黄金比
-  geom_line(data = ratio_df, 
-            mapping = aes(x = n, y = r)) + # 2項比
-  coord_cartesian(ylim = c(0, 2)) + # 表示サイズ
-  labs(title = "golden ratio", 
-       subtitle = parse(text = val_label), 
-       x = "n", y = "ratio")
 
 
